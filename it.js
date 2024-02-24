eca4e753cf6662cf2df9054f3a0daea55e38e350
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2024-02-24 12:51:11 +0600

    'module-17'

---
+++ it.js
@@ -0,0 +1,13 @@
+const number=[10, 23, 324, 24, 13, 342, 57, 87, 56, 334, 24, 96, 324 ]
+// console.log(number.length)   
+// number.push(1000)
+// number.shift(10)
+// console.log(number)
+console.log(number.includes(10090))
+
+if(number.includes(34)){
+    console.log('this is last number')
+}
+else{
+    console.log('this number is not identify')
+}
